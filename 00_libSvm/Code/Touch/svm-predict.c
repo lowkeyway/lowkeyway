236e9e8152ccd19572efa8a797ed18ea61361257
lowkeyway <lowkeyway@126.com>
2021-06-28 13:02:27 +0800

    Add model2C.py and modify predict.c

--- 00_libSvm/Code/Touch/svm-predict.c
+++ 00_libSvm/Code/Touch/svm-predict.c
@@ -10,13 +10,14 @@
 #include <limits.h>
 #include <stdbool.h>
 #include "svm.h"
+#include "svm_model.h"
 
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 
 static int (*info)(const char *fmt,...) = &printf;
 
 struct svm_node *x;
-int max_nr_attr = 64;
+int max_nr_attr = 64 * 3;
 
 struct svm_model* model;
 
@@ -258,56 +259,33 @@ void predict(FILE *input, FILE *output)
 		(double)correct/total*100,correct,total);
 }
 
+
+//
+// FSCANF helps to handle fscanf failures.
+// Its do-while block avoids the ambiguity when
+// if (...)
+//    FSCANF();
+// is used
+//
+static const char* svm_type_table[] =
+{
+	"c_svc","nu_svc","one_class","epsilon_svr","nu_svr",NULL
+};
+
+static const char* kernel_type_table[] =
+{
+	"linear","polynomial","rbf","sigmoid","precomputed",NULL
+};
+
+#define FSCANF(_stream, _format, _var) do{ if (fscanf(_stream, _format, _var) != 1) return false; }while(0)
+
+
 void dump_svm_model(struct svm_model* model)
 {
-#if 0
-	struct svm_parameter
-	{
-		int svm_type;
-		int kernel_type;
-		int degree;	/* for poly */
-		double gamma;	/* for poly/rbf/sigmoid */
-		double coef0;	/* for poly/sigmoid */
-
-		/* these are for training only */
-		double cache_size; /* in MB */
-		double eps;	/* stopping criteria */
-		double C;	/* for C_SVC, EPSILON_SVR and NU_SVR */
-		int nr_weight;		/* for C_SVC */
-		int* weight_label;	/* for C_SVC */
-		double* weight;		/* for C_SVC */
-		double nu;	/* for NU_SVC, ONE_CLASS, and NU_SVR */
-		double p;	/* for EPSILON_SVR */
-		int shrinking;	/* use the shrinking heuristics */
-		int probability; /* do probability estimates */
-	};
-
-	//
-	// svm_model
-	//
-	struct svm_model
-	{
-		struct svm_parameter param;	/* parameter */
-		int nr_class;		/* number of classes, = 2 in regression/one class svm */
-		int l;			/* total #SV */
-		struct svm_node** SV;		/* SVs (SV[l]) */
-		double** sv_coef;	/* coefficients for SVs in decision functions (sv_coef[k-1][l]) */
-		double* rho;		/* constants in decision functions (rho[k*(k-1)/2]) */
-		double* probA;		/* pariwise probability information */
-		double* probB;
-		int* sv_indices;        /* sv_indices[0,...,nSV-1] are values in [1,...,num_traning_data] to indicate SVs in the training set */
-
-		/* for classification only */
-
-		int* label;		/* label of each class (label[k]) */
-		int* nSV;		/* number of SVs for each class (nSV[k]) */
-					/* nSV[0] + nSV[1] + ... + nSV[k-1] = l */
-		/* XXX */
-		int free_sv;		/* 1 if svm_model is created by svm_load_model*/
-					/* 0 if svm_model is created by svm_train */
-	};
-#endif
 	int i = 0, j = 0;
+	double *coef = NULL;
+	struct svm_node *pNode = NULL;
+	struct svm_node node;
 
 	info("param.svm_type = %d\n", model->param.svm_type);
 	info("param.kernel_type = %d\n", model->param.kernel_type);
@@ -330,311 +308,63 @@ void dump_svm_model(struct svm_model* model)
 	info("nSV = [%d, %d]\n", model->nSV[0], model->nSV[1]);
 
 	info("sv_coef = ");
+	coef = model->sv_coef[0];
 	for (i = 0; i < model->l; i++)
-		info("%f ", model->sv_coef[0][i]);
+		info("%f ", *(coef + i));
 	info("\n");
 
-#if 0
-	info("SV = \n");
-	for (i = 0; i < model->l; i++, j = 0)
+	info("SV = ");
+	for (i = 0; i < model->l; i++)
 	{
-		while (-1 != model->SV[i][j].index)
-		{
-			info("%d:%f ", model->SV[i][j].index, model->SV[i][j].value);
-			j++;
-		}
-
+		pNode = model->SV[i];
+		for(j = 0; j < 10; j++)
+			info("%d:%f, ", pNode[j].index, pNode[j].value);
 		info("\n");
 	}
-#endif
+		
+	info("\n");
 }
 
-
-//
-// FSCANF helps to handle fscanf failures.
-// Its do-while block avoids the ambiguity when
-// if (...)
-//    FSCANF();
-// is used
-//
-static const char* svm_type_table[] =
+static struct svm_model* fill_model()
 {
-	"c_svc","nu_svc","one_class","epsilon_svr","nu_svr",NULL
-};
-
-static const char* kernel_type_table[] =
-{
-	"linear","polynomial","rbf","sigmoid","precomputed",NULL
-};
-
-#define FSCANF(_stream, _format, _var) do{ if (fscanf(_stream, _format, _var) != 1) return false; }while(0)
-static bool read_model_header(FILE* fp, struct svm_model* model)
-{
-	struct svm_parameter *param = &model->param;
-	// parameters for training only won't be assigned, but arrays are assigned as NULL for safety
-	param->nr_weight = 0;
-	param->weight_label = NULL;
-	param->weight = NULL;
-
-	char cmd[81];
-	while (1)
-	{
-		FSCANF(fp, "%80s", cmd);
-		LOU_P("%s\n", cmd);
-		if (strcmp(cmd, "svm_type") == 0)
-		{
-			FSCANF(fp, "%80s", cmd);
-			int i;
-			for (i = 0; svm_type_table[i]; i++)
-			{
-				if (strcmp(svm_type_table[i], cmd) == 0)
-				{
-					param->svm_type = i;
-					break;
-				}
-			}
-			if (svm_type_table[i] == NULL)
-			{
-				fprintf(stderr, "unknown svm type.\n");
-				return false;
-			}
-		}
-		else if (strcmp(cmd, "kernel_type") == 0)
-		{
-			FSCANF(fp, "%80s", cmd);
-			int i;
-			for (i = 0; kernel_type_table[i]; i++)
-			{
-				if (strcmp(kernel_type_table[i], cmd) == 0)
-				{
-					param->kernel_type = i;
-					break;
-				}
-			}
-			if (kernel_type_table[i] == NULL)
-			{
-				fprintf(stderr, "unknown kernel function.\n");
-				return false;
-			}
-		}
-		else if (strcmp(cmd, "degree") == 0)
-			FSCANF(fp, "%d", &param->degree);
-		else if (strcmp(cmd, "gamma") == 0)
-			FSCANF(fp, "%lf", &param->gamma);
-		else if (strcmp(cmd, "coef0") == 0)
-			FSCANF(fp, "%lf", &param->coef0);
-		else if (strcmp(cmd, "nr_class") == 0)
-			FSCANF(fp, "%d", &model->nr_class);
-		else if (strcmp(cmd, "total_sv") == 0)
-			FSCANF(fp, "%d", &model->l);
-		else if (strcmp(cmd, "rho") == 0)
-		{
-			int n = model->nr_class * (model->nr_class - 1) / 2;
-			model->rho = Malloc(double, n);
-			for (int i = 0; i < n; i++)
-				FSCANF(fp, "%lf", &model->rho[i]);
-		}
-		else if (strcmp(cmd, "label") == 0)
-		{
-			int n = model->nr_class;
-			model->label = Malloc(int, n);
-			for (int i = 0; i < n; i++)
-				FSCANF(fp, "%d", &model->label[i]);
-		}
-		else if (strcmp(cmd, "probA") == 0)
-		{
-			int n = model->nr_class * (model->nr_class - 1) / 2;
-			model->probA = Malloc(double, n);
-			for (int i = 0; i < n; i++)
-				FSCANF(fp, "%lf", &model->probA[i]);
-		}
-		else if (strcmp(cmd, "probB") == 0)
-		{
-			int n = model->nr_class * (model->nr_class - 1) / 2;
-			model->probB = Malloc(double, n);
-			for (int i = 0; i < n; i++)
-				FSCANF(fp, "%lf", &model->probB[i]);
-		}
-		else if (strcmp(cmd, "nr_sv") == 0)
-		{
-			int n = model->nr_class;
-			model->nSV = Malloc(int, n);
-			for (int i = 0; i < n; i++)
-				FSCANF(fp, "%d", &model->nSV[i]);
-		}
-		else if (strcmp(cmd, "SV") == 0)
-		{
-			while (1)
-			{
-				int c = getc(fp);
-				if (c == EOF || c == '\n') break;
-			}
-			break;
-		}
-		else
-		{
-			fprintf(stderr, "unknown text in model file: [%s]\n", cmd);
-			return false;
-		}
-	}
-
-	return true;
-
-}
-
-static void fill_model(struct svm_model* model)
-{
-	model->param.svm_type = 0;
-	model->param.kernel_type = 2;
-	model->param.gamma = 0.0078120000000000004;
-	model->nr_class = 2;
-	model->l = 19;
-	model->rho = Malloc(double, 1);
-	model->rho[0] = -0.0018908804852536274;
-	model->label = Malloc(int, 2);
-	model->label[0] = 1;
-	model->label[1] = -1;
-	model->nSV = Malloc(int, 2);
-	model->nSV[0] = 10;
-	model->nSV[1] = 9;
-}
-
-static struct svm_model* svm_load_model(const char* model_file_name)
-{
-	FILE* fp = fopen(model_file_name, "rb");
-	if (fp == NULL) return NULL;
-
-	char* old_locale = setlocale(LC_ALL, NULL);
-	if (old_locale) {
-		old_locale = strdup(old_locale);
-	}
-	setlocale(LC_ALL, "C");
-
-	// read parameters
-
 	struct svm_model* model = Malloc(struct svm_model, 1);
-	model->rho = NULL;
+	memset(model, 0, sizeof(struct svm_model));
+	int i;
 	model->probA = NULL;
 	model->probB = NULL;
 	model->sv_indices = NULL;
-	model->label = NULL;
-	model->nSV = NULL;
 
+	model->param.nr_weight = 0;
+	model->param.weight_label = NULL;
+	model->param.weight = NULL;
+	model->param.svm_type = SVM_TYPE;
+	model->param.kernel_type = KERNEL_TYPE;
+	model->param.gamma = GAMMA;
+	model->nr_class = NR_CLASS;
+	model->l = TOTAL_SV;
+	model->rho = rho;
+	model->label = label;
 
-	// read header
-	if (!read_model_header(fp, model))
-	{
-		fprintf(stderr, "ERROR: fscanf failed to read model\n");
-		setlocale(LC_ALL, old_locale);
-		free(old_locale);
-		free(model->rho);
-		free(model->label);
-		free(model->nSV);
-		free(model);
-		return NULL;
-	}
-
-	// read sv_coef and SV
-	fill_model(model);
-
-	int elements = 0;
-	long pos = ftell(fp);
-	LOU_P("pos = %ld\n", pos);
-	max_line_len = 2048;
-	line = Malloc(char, max_line_len);
-	char* p, * endptr, * idx, * val;
-
-	while (readline(fp) != NULL)
-	{
-		//LOU_P("elements[%d]: %s\n", elements, line);
-		p = strtok(line, ":");
-		if (160 > elements) LOU_P("%s\n", p);
-		while (1)
-		{
-			p = strtok(NULL, ":");
-			if (p == NULL)
-				break;
-			++elements;
-			if (160 > elements) LOU_P("%s\n", p);
-		}
-
-	}
-	elements += model->l;
-
-	fseek(fp, pos, SEEK_SET);
+	model->nSV = nr_sv;
 
 	int m = model->nr_class - 1;
-	int l = model->l;
 	model->sv_coef = Malloc(double*, m);
-	int i;
-	for (i = 0; i < m; i++)
-		model->sv_coef[i] = Malloc(double, l);
+	model->sv_coef[0] = (double*)&sv_coef[0];
+
+	int l = model->l;
 	model->SV = Malloc(struct svm_node*, l);
-	struct svm_node* x_space = NULL;
-	if (l > 0) x_space = Malloc(struct svm_node, elements);
-
-	int j = 0;
-	for (i = 0; i < l; i++)
-	{
-		readline(fp);
-		model->SV[i] = &x_space[j];
-
-		p = strtok(line, " \t");
-		model->sv_coef[0][i] = strtod(p, &endptr);
-		for (int k = 1; k < m; k++)
-		{
-			p = strtok(NULL, " \t");
-			model->sv_coef[k][i] = strtod(p, &endptr);
-		}
-
-		while (1)
-		{
-			idx = strtok(NULL, ":");
-			val = strtok(NULL, " \t");
-
-			if (val == NULL)
-				break;
-			x_space[j].index = (int)strtol(idx, &endptr, 10);
-			x_space[j].value = strtod(val, &endptr);
-
-			++j;
-		}
-		x_space[j++].index = -1;
-	}
-	free(line);
-
-	setlocale(LC_ALL, old_locale);
-	free(old_locale);
-
-	if (ferror(fp) != 0 || fclose(fp) != 0)
-		return NULL;
+	for(i = 0; i < l; i++)
+		model->SV[i] = (struct svm_node*)&SV[i];
 
 	model->free_sv = 1;	// XXX
+
 	return model;
 }
 
 
+
 void svm_free_model_content(struct svm_model* model_ptr)
 {
-	if (model_ptr->free_sv && model_ptr->l > 0 && model_ptr->SV != NULL)
-		free((void*)(model_ptr->SV[0]));
-	if (model_ptr->sv_coef)
-	{
-		for (int i = 0; i < model_ptr->nr_class - 1; i++)
-			free(model_ptr->sv_coef[i]);
-	}
-
-	free(model_ptr->SV);
-	model_ptr->SV = NULL;
-
-	free(model_ptr->sv_coef);
-	model_ptr->sv_coef = NULL;
-
-	free(model_ptr->rho);
-	model_ptr->rho = NULL;
-
-	free(model_ptr->label);
-	model_ptr->label = NULL;
 
 	free(model_ptr->probA);
 	model_ptr->probA = NULL;
@@ -645,8 +375,6 @@ void svm_free_model_content(struct svm_model* model_ptr)
 	free(model_ptr->sv_indices);
 	model_ptr->sv_indices = NULL;
 
-	free(model_ptr->nSV);
-	model_ptr->nSV = NULL;
 }
 
 static void svm_free_and_destroy_model(struct svm_model** model_ptr_ptr)
@@ -689,11 +417,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if((model=svm_load_model(argv[i+1]))==0)
-	{
-		fprintf(stderr,"can't open model file %s\n",argv[i+1]);
-		exit(1);
-	}
+	model = fill_model();
 	dump_svm_model(model);
 	x = (struct svm_node *) malloc(max_nr_attr*sizeof(struct svm_node));
 
